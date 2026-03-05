d58879b462f2b45c362dab20c01cee67d06cd5be
Francesco Zerbinato <francesco.zerbinato@gmail.com>
2026-03-05 12:27:06 +0100

    change title in php

--- wp-content/themes/hello-theme-child-master/thankyou/thankyou.js
+++ wp-content/themes/hello-theme-child-master/thankyou/thankyou.js
@@ -1,12 +1,6 @@
 ( function () {
 
 	function fixThankYouPage() {
-		// Cambia il titolo H1 Elementor hardcoded "Pagamento" → "Ordine ricevuto!"
-		var heading = document.querySelector( '.elementor-heading-title' );
-		if ( heading ) {
-			heading.textContent = 'Ordine ricevuto!';
-		}
-
 		// Nasconde il sottotitolo "Completa il pagamento..."
 		document.querySelectorAll( '.elementor-widget-text-editor p' ).forEach( function ( p ) {
 			if ( p.textContent.indexOf( 'Completa il pagamento' ) !== -1 ) {
--- wp-content/themes/hello-theme-child-master/thankyou/thankyou.php
+++ wp-content/themes/hello-theme-child-master/thankyou/thankyou.php
@@ -3,7 +3,13 @@
  * Thank You page customization
  */
 
-// Il titolo H1 Elementor è hardcoded nel widget → viene cambiato via JS (thankyou.js)
+// Cambia il titolo H1 Elementor hardcoded "Pagamento" → "Ordine ricevuto!" lato server
+add_filter( 'elementor/widget/render_content', function ( $content, $widget ) {
+	if ( $widget->get_name() === 'heading' && is_wc_endpoint_url( 'order-received' ) ) {
+		$content = str_replace( 'Pagamento', 'Ordine ricevuto!', $content );
+	}
+	return $content;
+}, 10, 2 );
 
 // Badge stato prenotazione dopo i meta di ogni item
 add_action( 'woocommerce_order_item_meta_end', function( $item_id, $item, $order, $plain_text ) {
