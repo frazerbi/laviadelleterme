88c1b5d937518787e5fe3dc7fdfa64ca00305c71
Francesco Zerbinato <francesco.zerbinato@gmail.com>
2026-03-20 15:50:17 +0100

    corrected body text for booking email confirmation

--- wp-content/plugins/plugin-custom-skianet/includes/class-booking-email-notification.php
+++ wp-content/plugins/plugin-custom-skianet/includes/class-booking-email-notification.php
@@ -43,6 +43,13 @@ class Booking_Email_Notification {
         add_action('woocommerce_order_actions', array($this, 'add_order_actions'));
         add_action('woocommerce_order_action_send_booking_confirmation', array($this, 'send_to_customer'));
         add_action('woocommerce_order_action_send_booking_confirmation_to_admin', array($this, 'send_to_admin'));
+
+        // Footer email
+        add_filter('woocommerce_email_footer_text', array($this, 'custom_email_footer_text'));
+    }
+
+    public function custom_email_footer_text() {
+        return 'La Via Delle Terme';
     }
 
     /**
@@ -196,26 +203,26 @@ class Booking_Email_Notification {
             // Costruisci dettaglio
             $detail = "<div style='margin-bottom: 15px; padding: 10px; border-left: 3px solid #0074A0;'>";
             $detail .= "<strong>Prodotto:</strong> <span style='color: #0074A0;'>" . $item->get_name() . "</span><br>";
-            $detail .= "<strong>Quantità:</strong> <span style='color: #0074A0;'>" . $item->get_quantity() . "</span><br>";
+            $detail .= "<strong>Quantità Coupon:</strong> <span style='color: #0074A0;'>" . $item->get_quantity() . "</span><br>";
             $detail .= "<strong>Totale:</strong> <span style='color: #0074A0;'>€" . $item->get_total() . "</span><br>";
-            
+
             // Codici
             if (!empty($codes)) {
                 $detail .= "<strong>Codici Coupon:</strong> <span style='color: #0074A0;'>" . implode(", ", $codes) . "</span><br>";
             }
-            
+
             // Dati prenotazione
-            $detail .= "<strong>Location:</strong> <span style='color: #0074A0;'>" . $booking_data['location_name'] . "</span><br>";
-            $detail .= "<strong>Data:</strong> <span style='color: #0074A0;'>" . $formatted_date . "</span><br>";
+            $detail .= "<strong>Location preferita:</strong> <span style='color: #0074A0;'>" . $booking_data['location_name'] . "</span><br>";
+            $detail .= "<strong>Data prenotazione:</strong> <span style='color: #0074A0;'>" . $formatted_date . "</span><br>";
 
             // Orario
             if (!empty($booking_data['time_slot_label'])) {
-                $detail .= "<strong>Orario:</strong> <span style='color: #0074A0;'>" . esc_html($booking_data['time_slot_label']) . "</span><br>";
+                $detail .= "<strong>Orario prenotazione:</strong> <span style='color: #0074A0;'>" . esc_html($booking_data['time_slot_label']) . "</span><br>";
             }
 
             $ticket_labels = array('4h' => '4 Ore', 'giornaliero' => 'Giornaliero', 'serale' => 'Serale');
             $detail .= "<strong>Tipo Ingresso:</strong> <span style='color: #0074A0;'>" . ($ticket_labels[$booking_data['ticket_type']] ?? $booking_data['ticket_type']) . "</span><br>";
-            
+
             // Ospiti
             if ($booking_data['num_male'] > 0) {
                 $detail .= "<strong>Uomini:</strong> <span style='color: #0074A0;'>" . $booking_data['num_male'] . "</span><br>";
@@ -223,7 +230,9 @@ class Booking_Email_Notification {
             if ($booking_data['num_female'] > 0) {
                 $detail .= "<strong>Donne:</strong> <span style='color: #0074A0;'>" . $booking_data['num_female'] . "</span><br>";
             }
-            
+
+            $detail .= "<strong>Scadenza Coupon:</strong> <span style='color: #0074A0;'>sei mesi dalla data di emissione del Coupon</span><br>";
+
             $detail .= "</div>";
             
             $order_details[] = $detail;
@@ -239,28 +248,30 @@ class Booking_Email_Notification {
         $order_details_text = implode("<br>", $order_details);
 
         // Saluto con nome cliente
-        $greeting = "<p>Grazie per la tua prenotazione.";
+        $greeting = "<p>Grazie per la tua prenotazione presso le nostre Strutture.";
         if ($order) {
             $first_name = $order->get_billing_first_name();
             $last_name = $order->get_billing_last_name();
             if ($first_name || $last_name) {
-                $greeting = "<p>Gentile <strong>" . trim($first_name . ' ' . $last_name) . "</strong>, grazie per la tua prenotazione.";
+                $greeting = "<p>Gentile <strong>" . trim($first_name . ' ' . $last_name) . "</strong>, grazie per la tua prenotazione presso le nostre Strutture.";
             }
         }
 
         $policy_info = "<br><br><strong>Dichiarazioni e Informative:</strong><br>";
-        $policy_info .= "<p>Il Sottoscritto/a <strong>DICHIARA</strong> sotto la propria responsabilità di avere preso visione delle norme comportamentali per i servizi offerti dalla struttura, a disposizione alla reception della spa, ed in particolare:</p>";
+        $policy_info .= "<p>Il/la Sottoscritto/a dichiara, sotto la propria responsabilità, di avere preso visione del Regolamento per l'accesso ai servizi offerti dalle nostre strutture scaricabile al link: <a href=\"https://laviadelleterme.it/regolamento-strutture/\">https://laviadelleterme.it/regolamento-strutture/</a>, e di:</p>";
         $policy_info .= "<ul>";
-        $policy_info .= "<li>Di essere a conoscenza che l'uso di sauna e bagno turco non sono idonei a coloro che hanno disturbi di pressione arteriosa e presenza di patologia a carico del sistema venoso superficiale e profondo.</li>";
-        $policy_info .= "<li>Dichiara altresì di godere di sana e robusta costituzione e di essersi sottoposto di recente a visita medica per accertare la propria idoneità fisica ed esonera pertanto la struttura da qualsiasi responsabilità.</li>";
-        $policy_info .= "<li>Dichiara di non accusare sintomi quali: febbre, tosse, difficoltà respiratorie.</li>";
-        $policy_info .= "<li>Dichiara di non aver soggiornato in zone e/o Paesi con presunta trasmissione comunitaria.</li>";
+        $policy_info .= "<li>essere a conoscenza che l'utilizzo di sauna e bagno turco non sono idonei per persone con disturbi di pressione arteriosa e la presenza di patologie a carico del sistema venoso superficiale e profondo.</li>";
+        $policy_info .= "<li>godere di sana e robusta costituzione e di essersi sottoposto di recente a visita medica per accertare la propria idoneità fisica ed esonera pertanto la struttura da qualsiasi responsabilità.</li>";
         $policy_info .= "</ul>";
-        $policy_info .= "<p><strong>Politica di cancellazione:</strong> È possibile cancellare la prenotazione fino a 48 ore prima tramite il seguente link: <a href=\"https://www.termegest.it/annullapren.aspx\">https://www.termegest.it/annullapren.aspx</a>. Dopo tale termine non sarà più possibile annullarla e il coupon verrà riscattato automaticamente.</p>";
-        $policy_info .= "<p>Ai sensi degli articoli 13 e 23 del DLgs. 196/03, del Regolamento EU 679/2016 e del Dlgs 101/2018 (Codice sulla protezione dei dati personali), l'interessato dichiara di essere stato adeguatamente informato ed esprime il proprio consenso all'utilizzo dei dati personali che lo riguardano, con particolare riferimento ai dati che la legge definisce come \"sensibili\", nei limiti di quanto indicato nell'informativa.</p>";
-        $policy_info .= "<p>Dichiaro di avere preso visione del regolamento interno che definisce le modalità per la custodia dei valori accettandone i contenuti.</p>";
+        $policy_info .= "<p><strong>Cambio della Struttura prenotata</strong><br>";
+        $policy_info .= "L'acquisto del Coupon le dà diritto di modificare la scelta della Struttura qui prenotata con un'altra delle nostre due strutture che trova nella nostra Home page fino a 48 ore precedenti alla data della prenotazione, salvo disponibilità. Dopo non sarà possibile modificare la scelta della Struttura.</p>";
+        $policy_info .= "<p><strong>Cancellazione e modifica della Prenotazione</strong><br>";
+        $policy_info .= "La sua prenotazione è cancellabile, o modificabile, fino a 48 ore precedenti alla data della prenotazione. Dopo non sarà possibile modificarla.<br>";
+        $policy_info .= "Per cancellare la prenotazione accedi al link: <a href=\"https://www.termegest.it/annullapren.aspx\">https://www.termegest.it/annullapren.aspx</a></p>";
+        $policy_info .= "<p><strong>Privacy</strong><br>";
+        $policy_info .= "Ai sensi degli articoli 13 e 23 del DLgs. 196/03, del Regolamento EU 679/2016 e del Dlgs 101/2018 (Codice sulla protezione dei dati personali), l'interessato dichiara di essere stato adeguatamente informato ed esprime il proprio consenso all'utilizzo dei dati personali che lo riguardano, con particolare riferimento ai dati che la legge definisce come \"sensibili\", nei limiti di quanto indicato nell'informativa.</p>";
 
-        $email_body = $greeting . " Di seguito i dettagli dell'ordine:</p><br>";
+        $email_body = $greeting . " Ecco i dettagli del tuo Coupon:</p><br>";
         $email_body .= $order_details_text . $policy_info;
         
         return $email_body;
